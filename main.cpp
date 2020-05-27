c1b960131e7df54c7903c68eb93b67eb1c8dc03c
Kirill_21 <benzilacetat@outlook.com>
2020-05-27 13:52:57 +0300

    Доработки

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include <cstring>
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fstream>
 #include <string>
 #include <vector> 
 #include <sstream>
+#include <map>
+
+#include <string.h>
 
 using namespace std;
 int main() {
@@ -84,11 +90,27 @@ int main() {
     } // variant 1
 
 if (variant == 2){
-
+    
+    cout << "Введите известные цифры номера, неизвестные замените на '*'" << endl;
+    string phone;
+    cin >> phone;
+    
+    map <string, string> mp; // map состоит из ключа - порядка цифры, и значения - значения цифры
+    string key = "";
+    for (int i = 0; i < phone.size(); i++){
+        if (phone[i] != '*'){
+            key = i;
+            mp[key] = phone[i];
+            cout << mp[key] << endl;
+        }
+    }
+    
     string line;
     vector<string> number1_vector;
     vector<string> number2_vector;
 
+    // первый файл с номерами
+    
     ifstream myfile("/Users/kirill/Documents/C++/Wifi/Data/phone_codes.txt"); // окрываем файл для чтения
     if (myfile.is_open())
     {
@@ -99,9 +121,10 @@ if (variant == 2){
         }
     }
     myfile.close();     // закрываем файл
-     
     cout << "Закрыт первый файл" << endl;
 
+    // второй файл с номерами
+    
     ifstream fin("/Users/kirill/Documents/C++/Wifi/Data/next_phone_codes.txt"); // окрываем файл для чтения
     if (fin.is_open())
     {
@@ -112,28 +135,44 @@ if (variant == 2){
         }
     }
     fin.close();     // закрываем файл
-     
     cout << "Закрыт второй файл" << endl;
 
-
+    
     cout << "Введите SSID" << endl;
     //cin >> SSID;
     string a = "";
+    bool err = 0;
     //cin >> password;
     for (int i = 0; i < number1_vector.size(); i++){
         cout << "Цикл по номерам\n";
         for (int j = 0; j < number2_vector.size(); j++){
-            password = number1_vector[i] + number2_vector[j];
+            password = number1_vector[i] + number2_vector[j]; // пароль номер целиком
+            
+            err = 0;
+            
+            for (int k = 0; k < password.size(); k++){ // прохождение по номеру
+                key = k; // to str
+                
+                if (strcmp(password[k],mp[key]) != 0) { // поиск совпадений
+                    err = 1;
+                    break;
+                }
+            }
+            
+            if (err == 0){ // при наличии совпадений в цифрах номера выполнить
+                
+                cout << "Пароль " << password << endl;
 
-            cout << "Пароль " << password << endl;
+                ofstream fout;
+                fout.open("/Users/kirill/Documents/C++/Wifi/start.sh");
+                fout << "#!/usr/bin/env bash" << endl;
+                fout << "networksetup -setairportnetwork en0 " << SSID << " " << password << "; echo \"\n\"" <<endl;
+                fout.close();
 
-            ofstream fout;
-            fout.open("/Users/kirill/Documents/C++/Wifi/start.sh");
-            fout << "#!/usr/bin/env bash" << endl;
-            fout << "networksetup -setairportnetwork en0 " << SSID << " " << password << "; echo \"\n\"" <<endl;
-            fout.close();
-
-            system("/Users/kirill/Documents/C++/Wifi/start.sh");
+                system("/Users/kirill/Documents/C++/Wifi/start.sh");
+                
+            }
+            
         }
     }
 
